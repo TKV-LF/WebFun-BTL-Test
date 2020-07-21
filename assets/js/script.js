2b2a6fb62cd78dee89082887e2265d1dae34df73
unknown <1851161554@e.tlu.edu.vn>
2020-07-21 23:06:26 +0700

    up thu

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#btnSearch").click(function () { 
+        alert("adasds");
+        $("#searchForm").css("display", "block !important");        
+    });
+});
